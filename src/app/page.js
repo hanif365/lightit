8a73c237e508e316ac1014c85ae63ab42d87f862
hanif365 <m.a.hanifkhan64@gmail.com>
2024-03-18 15:43:27 +0600

    res: make responsive of Service section

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="">
       <HeroSection />
-      {/* <Services /> */}
+      <Services />
       {/* <Awards />
       <Industries />
       <SuccessStories />
--- src/components/HomePageComponent/Services/Services.jsx
+++ src/components/HomePageComponent/Services/Services.jsx
@@ -3,24 +3,26 @@ import serviceData from "../../../data/servicesData.json";
 
 const Services = () => {
   return (
-    <div className="px-14 2xl:px-72 pt-20 pb-24 bg-[#f1f4fd]">
-      <h1 className="lg:text-5xl 2xl:text-6xl font-bold text-center">
+    <div className="px-5 lg:px-14 2xl:px-72 pt-20 pb-24 bg-[#f1f4fd]">
+      <h1 className="text-3xl lg:text-5xl 2xl:text-6xl font-bold text-center">
         Services we provide
       </h1>
-      <div className="grid grid-cols-3 gap-x-6 gap-y-10 pt-20 ">
+      <div className="grid lg:grid-cols-3 gap-x-6 gap-y-3 lg:gap-y-10 pt-10 lg:pt-20">
         {serviceData.map((service) => (
           <div
             key={service.id}
-            className="bg-white rounded-2xl p-8 cursor-pointer hover:drop-shadow-2xl"
+            className="bg-white rounded-2xl p-4 lg:p-8 cursor-pointer lg:hover:drop-shadow-2xl flex lg:block"
           >
-            <h2 className="lg:text-3xl font-bold">{service.name}</h2>
-            <p className="lg:text-lg tracking-wider py-5">
+            <h2 className="text-lg lg:text-3xl font-bold">{service.name}</h2>
+            <p className="hidden lg:block lg:text-lg tracking-wider py-5">
               {service.description}
             </p>
             <div className="flex items-center text-red-500 hover:text-red-600 group">
-              <p className="mr-5 font-medium">Learn more</p>
+              <p className="invisible lg:visible mr-2 lg:mr-5 font-medium">
+                Learn more
+              </p>
               <FaArrowRightLong
-                className="group-hover:ml-3 duration-300"
+                className="lg:group-hover:ml-3 duration-300 absolute lg:static right-10 lg:right-0"
                 size={20}
               />
             </div>
